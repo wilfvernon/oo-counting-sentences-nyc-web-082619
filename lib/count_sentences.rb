f8c5a2e4be03e1d5c1f7dc061da85fb5799cab5e
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 21:43:38 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    arr = self.split(/[!.?]/)
+    arr.reject!{|arr|arr.empty?}
+    arr.length
   end
 end
